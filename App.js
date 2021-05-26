a2e90da15218fad1ae4889e8d28f33e0bac05cb2
nurgnc <gencbedriyenur@gmail.com>
2021-05-26 22:38:03 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { SafeAreaView, View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native'
+
+export default class App extends React.Component {
+  contructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+
+  }
+  render() {
+
+    return (
+      <SafeAreaView>
+        <View style={style.}>
+          <View>
+
+          </View>
+          <View>
+
+          </View>
+        </View>
+      </SafeAreaView>
+    )
+  }
+}
+
+
+const style = StyleSheet.create({
+
+})
